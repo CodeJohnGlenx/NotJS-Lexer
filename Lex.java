caa5eb4b82aa30b59ecd64c6066aaac2fc22afd2
John Glen Falceso <johnglenfalceso@protonmail.com>
2023-01-06 21:12:16 +0800

    creates LexOut.txt after successfully scanning the lexemes.

--- Lex.java
+++ Lex.java
@@ -1,18 +1,19 @@
 import java.util.regex.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+import java.util.ArrayList;
+import java.io.FileWriter;
 
 public class Lex {
     public static void main(String[] args) {
         // args[0] expects <filename>.njs
-        
         
         try {
             // convert .njs file into Java String
             String njsContent = Files.readString(Path.of(args[0]));
             String fileExtension = args[0].substring(args[0].indexOf('.'));
             njsContent += " ";
+            ArrayList<String[]> lexemeTokenList;
 
             // throw Exception if file extension is not ".njs"
             if (!fileExtension.equals(".njs")) {
@@ -20,7 +21,16 @@ public class Lex {
             }
 
             // perform scanning
-            scan(njsContent, Tokens.PATTERNLIST);
+            lexemeTokenList = scan(njsContent, Tokens.PATTERNLIST);
+            if (lexemeTokenList != null) {
+                // write lexeme and token to SymbolTable.txt
+                FileWriter lexWriter = new FileWriter("LexOut.txt");
+                for (String[] lexemeToken: lexemeTokenList) {
+                    lexWriter.write(String.format("%-50s%s\n", lexemeToken[0], lexemeToken[1]));
+                }
+                lexWriter.close();
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -29,9 +39,10 @@ public class Lex {
     // Scanning of Token
     // content - the string the will be scanned
     // patternList - a list of regular expressions 
-    public static int scan(String content, String[] patternList) {
+    public static ArrayList<String[]> scan(String content, String[] patternList) {
         Pattern pattern;
         Matcher matcher;
+        ArrayList<String[]> lexemeTokenList = new ArrayList<>();
 
         // identiferPattern and identifierMatcher for special cases
         Pattern identifierPattern = Pattern.compile("^[_a-zA-Z]+\\w*");
@@ -57,10 +68,12 @@ public class Lex {
                     // lexeme is a (keyword/reserved_word/bool_literal)
                     if (identifierSubString.equals(matcherSubString)) {
                         System.out.printf("%-50s%s\n", content.substring(matcher.start(), matcher.end()), Tokens.TOKEN.get(p));
+                        lexemeTokenList.add(new String[] {content.substring(matcher.start(), matcher.end()), Tokens.TOKEN.get(p)});
                         content = content.substring(matcher.end());
                     // lexeme is an identifier
                     } else {
                         System.out.printf("%-50s%s\n", content.substring(identifierMatcher.start(), identifierMatcher.end()), Tokens.TOKEN.get(identifierPattern.pattern()));
+                        lexemeTokenList.add(new String[] {content.substring(identifierMatcher.start(), identifierMatcher.end()), Tokens.TOKEN.get(identifierPattern.pattern())});
                         content = content.substring(identifierMatcher.end());
                     }
                     found = true;
@@ -71,6 +84,7 @@ public class Lex {
                 if (matcher.find(0) == true) {
                     if (Tokens.TOKEN.get(p) != "WHITESPACE" && Tokens.TOKEN.get(p) != "SINGLE_LINE_COMMENT" && Tokens.TOKEN.get(p) != "MULTI_LINE_COMMENT") {
                         System.out.printf("%-50s%s\n", content.substring(matcher.start(), matcher.end()), Tokens.TOKEN.get(p));
+                        lexemeTokenList.add(new String[] {content.substring(matcher.start(), matcher.end()), Tokens.TOKEN.get(p)});
                     }
                     content = content.substring(matcher.end());
                     found = true;
@@ -81,12 +95,15 @@ public class Lex {
             
             // symbol or lexeme is unrecognizable
             if (found == false) {
-                System.out.printf("%snot found\n", content);
-                System.out.println();
-                return -1;
+                String error = String.format("%s", content.charAt(0));
+                System.out.printf("%-50s%s\n", error, "NOT_RECOGNIZED_AS_TOKEN");
+                System.out.println("\n/*Lex Scanner Interrupted.*/\n");
+
+                //return new ArrayList<>() {{add(new String[] {error, "NOT_RECOGNIZED_AS_TOKEN"});}};
+                return null;
             }
         }
-        return 1;
+        return lexemeTokenList;
     }
 }
 
